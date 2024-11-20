d756cf1e0279d997641802ba7f08b6df0972e957
danieldcsoares <daniel.dayrellcs@gmail.com>
2024-11-20 01:04:22 -0300

    exerciciosresolvidos

---
+++ 03_bibliotecas/bibli_01/Resultados/Daniel/completo/main.c
@@ -0,0 +1,12 @@
+#include "esfera_utils.h"
+#include <stdio.h>
+
+int main(){
+    float raio;
+    
+    scanf("%f", &raio);
+    printf("Area: %.2f\nVolume: %.2f\n", calcula_area(raio), calcula_volume(raio));
+
+    return 0;
+
+}
